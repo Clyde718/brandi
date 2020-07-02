201d5071d840ff07c69c4f21d7b88c2da86f3f51
Kiril <kiril.bogdan.97@gmail.com>
2020-07-03 00:42:36 +0300

    Initial Commit

--- app/js/app.js
+++ app/js/app.js
@@ -12,4 +12,82 @@ $(document).ready(function () {
     $('.header__burger,.header__nav').toggleClass('active');
     $('body').toggleClass('lock');
   });
+
+
+
+
+  $('.features__row').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    appendDots: $('.features__dots'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.team__row').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    appendDots: $('.team__dots'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+  $('.filter__item').on('click', function (event) {
+    let i = $(this).data('filter');
+
+    console.log(i);
+
+    if (i == 1) {
+      $('.works__column').show();
+    } else {
+      $('.works__column').hide();
+      $('.works__column.f_' + i).show();
+    }
+
+    $('.filter__item').removeClass('active');
+    $(this).addClass('active')
+  })
 });
